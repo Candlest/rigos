2f6b1b52bc21816d3cda2f5674ae73d7db617486
Candlest <hiriset312@gmail.com>
2023-07-23 00:58:20 +0800

    <refactor>(server): Migrate server to web_server
    Since I'm rusty with Rust's features and the way the project is
    organized, I've temporarily chosen to use the `web_server` library to
    refactor the server module.
    
    fix #1

--- src/options/server.rs
+++ src/options/server.rs
@@ -1,47 +1,35 @@
-use std::io::{BufReader, Write, BufRead, self, Read};
+use percent_encoding::percent_decode;
+use web_server;
 
-use crate::utils;
+use crate::utils::{self, PUBLIC_DIR, info};
 /*Listen TCP */
 pub fn run_server(){
-    let listener = std::net::TcpListener::bind("127.0.0.1:7878").unwrap();
-    utils::info(utils::Info::RUN, "at", "http://localhost:7878");
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-         std::thread::spawn(|| {
-            handle_connection(stream);
-        });
-    }
-}
+    info(utils::Info::RUN, "run at", "http://localhost:7878");
+    info(utils::Info::RUN, "you can exit with", "CTRL + C");
 
-/*Tackle HTTP 
-Read the uri of the HTTP request and return the matching file content:
-For requests to /, index.html is returned
-For file requests, return the content if it exists, or return 404.html if it does not exist
-*/
-fn handle_connection(mut stream: std::net::TcpStream){
-    let buf_reader = std::io::BufReader::new(&mut stream);
-    let request_line = std::io::BufRead::lines(buf_reader).next().unwrap().unwrap();
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "/index.html".to_string()),
-        _ => {
-            let url_phase: Vec<&str> = request_line[..].split(' ').collect();
-            let url_raw = url_phase[1]; /*get uri */
-            let iter = percent_encoding::percent_decode(url_raw.as_bytes());
-            let decoded = iter.decode_utf8().unwrap().to_string();
-            let path_str = format!("{}{}", utils::PUBLIC_DIR, decoded);
-            let path_str = path_str.as_str();
-            if std::path::Path::exists(std::path::Path::new(path_str)){
-                ("HTTP/1.1 200 OK", decoded)
-            }
-            else {
-                crate::utils::info(utils::Info::RUN, "cannot found", &decoded);
-                ("HTTP/1.1 404 NOT FOUND", "/404.html".to_string())
-            }
-        }
-    };
-
-    let contents = std::fs::read_to_string(format!("{}{}", utils::PUBLIC_DIR, filename).as_str()).unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    std::io::Write::write_all(&mut stream, response.as_bytes()).unwrap();
+    /*
+     * 目前仍然没有解决Route
+     */
+    web_server::new()
+    .not_found(Box::new(|req, _|{
+        info(utils::Info::RUN, "cannot find", req.get_path().as_str());
+        std::path::Path::new(format!("{}/404.html", PUBLIC_DIR).as_str()).into()
+    }))
+    .get("/",Box::new(|_, _|{
+        std::path::Path::new(format!("{}/index.html", PUBLIC_DIR).as_str()).into()
+    }))
+    .get("/:f", Box::new(|request, _| {
+        info(utils::Info::RUN, "require file", request.get_path().as_str());
+        std::path::Path::new(format!("{}{}", PUBLIC_DIR, request.get_path()).as_str()).into()
+    }))
+    .get("/Post/:f", Box::new(|request, _| {
+        let decoded = percent_decode(request.get_path().as_str().as_bytes()).decode_utf8().unwrap().to_string();
+        info(utils::Info::RUN, "require file", &decoded);
+        std::path::Path::new(&format!("{}{}", PUBLIC_DIR, decoded)).into()
+    }))
+    // .get("/Resource:f", Box::new(|request, _| {
+    //     info(utils::Info::RUN, "require file", request.get_path().as_str());
+    //     std::path::Path::new(format!("{}{}", PUBLIC_DIR, request.get_path()).as_str()).into()
+    // }))
+   .launch(7878);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -23,20 +23,11 @@ pub const STATIC_DIR: &str = "static";
 pub const PAGE_DIR: &str = "page";
 pub const TEMPLATE_DIR: &str = "template";
 
-/*PATH */
-pub fn path_web2local(web_path: &str) -> String{
-    let iter = percent_encoding::percent_decode(web_path.as_bytes());
-    let decoded = iter.decode_utf8().unwrap();
-    let path_str = format!("{}{}", PUBLIC_DIR, decoded);
-    path_str
-}
-
+/* Thanks to web_server 
+ * We never use percent_encode again :-)
+ */
 pub fn path_local2web(local_path: &str) -> String{
-    let path = std::path::Path::new(local_path);
-    let filename = path.file_name().unwrap();
-    let result = &local_path[PUBLIC_DIR.len()..local_path.len() - &filename.len()]; /*非常方便！！！ */
-    let filename = percent_encoding::utf8_percent_encode(filename.to_str().unwrap(), percent_encoding::NON_ALPHANUMERIC).to_string();
-    format!("{}{}", result, filename)
+    local_path[PUBLIC_DIR.len()..].to_string() /*非常方便！！！ */
 }
 
 pub fn path_root2pub(raw_path: &str) -> (String, String){
